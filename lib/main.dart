f61154132a4beb45f353ec6987c69b47088f8f06
moohamed154 <medohs770@gmail.com>
2024-07-11 19:03:05 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+void main() async{
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(ScholarChat());
 }
 
@@ -13,7 +19,7 @@ class ScholarChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        'LoginnnPage' :(context) => LoginPage(),
+        'LoginPage' :(context) => LoginPage(),
         RegisterPage.id :(context) => RegisterPage(),
       },
       debugShowCheckedModeBanner: false,
